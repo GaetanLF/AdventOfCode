76033239674763e4672fc990c486c4e08978262d
Gaëtan LF <61993231+GaetanLF@users.noreply.github.com>
2023-12-04 17:57:26 +0100

    Add files via upload

--- Day1/main.py
+++ Day1/main.py
@@ -16,14 +16,10 @@ with open('list_of_inputs.txt') as f:
     data = f.readlines()
     
 modified = []
+sum_el = 0
 for i in data:
     j = re.sub("[^0-9]", "",i)
-    j = j[0] + j[-1]
-    modified.append(j)
-    
-sum_el = 0
-for i in modified:
-    sum_el += int(i)
+    sum_el += int(j[0] + j[-1]) # First and last digits
     
 print(sum_el) # Here it is.
     
